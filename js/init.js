0ca4f82fe9a79c9d7cf4fcbec3970bfc61b5874a
rickmreyes1969 <117231590+rickmreyes1969@users.noreply.github.com>
2022-11-01 16:19:32 -0600

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+    apiKey: "AIzaSyDdapj3H3Dd2w9DHpLHwbzwpwSwU7rItpg",
+    authDomain: "chatrickfb.firebaseapp.com",
+    projectId: "chatrickfb",
+    storageBucket: "chatrickfb.appspot.com",
+    messagingSenderId: "815561540833",
+    appId: "1:815561540833:web:2cf0f52c625e8e281723a0",
+    measurementId: "G-5W02XQR53J"
+    });
